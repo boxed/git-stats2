3ae0ba92ebdd9e4ec612570819be333e850f6706
Anders Hovmöller <boxed@killingar.net>
2015-05-01 14:54:00 +0200

    Basic graph

--- git_stats2.py
+++ git_stats2.py
@@ -1,10 +1,13 @@
+import os
 from collections import defaultdict
 from datetime import date
-import os
-from pygit2 import Repository, GIT_SORT_TOPOLOGICAL, GIT_OBJ_COMMIT, GIT_SORT_REVERSE
 from cPickle import dump, load
+from pygit2 import Repository, GIT_SORT_TOPOLOGICAL, GIT_OBJ_COMMIT, GIT_SORT_REVERSE
 
 cache_filename = 'repo-stats.pickle'
+
+limit_number_of_commits = None
+
 
 def defaultdict_int():
     return defaultdict(int)
@@ -13,26 +16,25 @@ if not os.path.exists(cache_filename):
     repo = Repository('django')
 
     data = {
-        'additions_by_date_by_author': defaultdict(defaultdict_int),
-        'deletions_by_date_by_author': defaultdict(defaultdict_int),
+        'author_to_date_to_additions': defaultdict(defaultdict_int),
+        'author_to_date_to_deletions': defaultdict(defaultdict_int),
     }
 
     count = 0
 
     last_commit = None
     for commit in repo.walk(repo.head.target, GIT_SORT_TOPOLOGICAL | GIT_SORT_REVERSE):
-        # commit.author.email
-        if count > 5000:
-            break
-        count += 1
         if commit.type == GIT_OBJ_COMMIT:
+            if limit_number_of_commits is not None and count > limit_number_of_commits:
+                break
+            count += 1
             d = repo.diff(commit, last_commit)
             patches = list(d)
             additions = sum([p.additions for p in patches])
             deletions = sum([p.deletions for p in patches])
-            data['additions_by_date_by_author'][date.fromtimestamp(commit.commit_time)][commit.author.email] += additions
-            data['additions_by_date_by_author'][date.fromtimestamp(commit.commit_time)][commit.author.email] += deletions
-        last_commit = commit
+            data['author_to_date_to_additions'][commit.author.email][date.fromtimestamp(commit.commit_time)] += additions
+            data['author_to_date_to_deletions'][commit.author.email][date.fromtimestamp(commit.commit_time)] += deletions
+            last_commit = commit
 
     with open(cache_filename, 'w') as f:
         dump(data, f)
@@ -40,3 +42,9 @@ else:
     with open(cache_filename) as f:
         data = load(f)
 
+with open('series.js', 'w') as output:
+    output.write('var series = [')
+    for author, date_to_additions in data['author_to_date_to_additions'].items():
+        data_points = ', \n'.join(['[Date.UTC(%s, %s, %s), %s]' % (day.year, day.month - 1, day.day, additions) for day, additions in date_to_additions.items()])
+        output.write("""{name: '%s', data: [%s]},""" % (author, data_points))
+    output.write('];')
